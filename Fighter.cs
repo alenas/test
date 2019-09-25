0d977f1ca4ff29c0ca57787691150acd8ab34d19
Alenas Kisonas <alenas@hotmail.com>
2019-09-25 12:11:22 +0300

    working

---
+++ Fighter.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Threading;
+
+namespace test {
+
+	public class Fighter {
+
+		public string Name { get; }
+
+		public int Health { get; private set; }
+
+		public int Damage { get; }
+
+		public bool IsDead { get { return Health <= 0; } }
+
+		private ConcurrentDictionary<string, Fighter> fighters;
+
+		public const int SleepIntervalMs = 500;
+
+		private static Random random = new Random((new Random()).Next(int.MaxValue));
+
+		public Fighter(string name, int health, int damage) {
+			Name = name;
+			Health = health;
+			Damage = damage;
+		}
+
+		/// <summary>
+		/// inflicts damage and returns if fighter is dead
+		/// </summary>
+		/// <param name="damage">Damage to inflict</param>
+		/// <returns>true if dead</returns>
+		public bool InflictDamage(int damage) {
+			Health -= damage;
+			if (IsDead) Log("is dead");
+			return IsDead;
+		}
+
+		public void StartShootingAt(ref ConcurrentDictionary<string, Fighter> fighters) {
+			Log("starts shooting");
+			this.fighters = fighters;
+			while (ShootRandom()) {
+				Thread.Sleep(SleepIntervalMs);
+			}
+		}
+
+		void Log(string action) {
+			Console.WriteLine($"{Name}({Health}): {action}");
+		}
+
+
+		/// <summary>
+		/// Shoots random player
+		/// </summary>
+		/// <returns>true if we can shoot again</returns>
+		bool ShootRandom() {
+			if (IsDead) {
+				// fighter is already dead
+				return false;
+			}
+			var fighter = GetSuitable();
+			if (fighter != null) {
+				Log($"shoots at {fighter.Name} {fighter.Health}-{Damage}");
+				if (fighter.InflictDamage(Damage)) {
+					fighters.TryRemove(fighter.Name, out _);
+				}
+				return true;
+			} else {
+				return false;
+			}
+
+		}
+
+		Fighter GetSuitable() {
+			do {
+				var fighter = GetRandom();
+				if (fighter != this && !fighter.IsDead) {
+					return fighter;
+				}
+			} while (fighters.Count > 1);
+
+			return null;
+		}
+
+		Fighter GetRandom() {
+			var i = random.Next(fighters.Count);
+			return fighters.ElementAt(i).Value;
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Newtonsoft.Json;
+
+namespace test {
+	public class Program {
+
+		readonly static string json = @"[
+			{
+			  'name': 'John',
+			  'health': 10,
+			  'damage': 1
+			},
+			{
+			  'name': 'Bill',
+			  'health': 8,
+			  'damage': 2
+			},
+			{
+			  'name': 'Sam',
+			  'health': 10,
+			  'damage': 1
+			},
+			{
+			  'name': 'Peter',
+			  'health': 5,
+			  'damage': 3
+			},
+			{
+			  'name': 'Philip',
+			  'health': 15,
+			  'damage': 1
+			}
+		]";
+
+
+		public static void Main(string[] args) {
+			var fighters = Deserialize(json);
+			// without setting a degree of parallelism, it would use only the same number as number of cores
+			fighters.AsParallel().WithDegreeOfParallelism(fighters.Count).ForAll(f => f.Value.StartShootingAt(ref fighters));
+			while (fighters.Count > 1) {
+				Thread.Sleep(500);
+			}
+			Console.WriteLine("Winner is: " + fighters.First().Value.Name);
+			Console.ReadKey();
+		}
+
+		static ConcurrentDictionary<string, Fighter> Deserialize(string json) {
+			var fighters = JsonConvert.DeserializeObject<List<Fighter>>(json);
+			ConcurrentDictionary<string, Fighter> result = new ConcurrentDictionary<string, Fighter>(fighters.Count, fighters.Count);
+			foreach (var f in fighters) {
+				result.TryAdd(f.Name, f);
+			}
+			return result;
+		}
+	}
+}
